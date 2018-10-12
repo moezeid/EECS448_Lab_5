bcbdd0028c17d00220ee05631c7355350c08e754
moezeid <m.zeid97@ku.edu>
2018-10-11 22:43:11 -0500

    "ADDED: fully functional user input and error reporting"

--- exercise3/CreateUser.php
+++ exercise3/CreateUser.php
@@ -4,6 +4,9 @@ $content = $_POST['post_content'];
 $mysqli = new mysqli("mysql.eecs.ku.edu", "m113z398", "peachfuzz", "m113z398");
 
 
+
+
+
 if($mysqli->connect_error){
     die("Connection failed: " . $mysqli->connect_error);
 
@@ -11,16 +14,33 @@ if($mysqli->connect_error){
 
     }
 
-  $query = "INSERT INTO User VALUES ('$u_id')";
+      $u_idQuery = "SELECT user_id FROM User WHERE user_id ='$u_id'";
+      $contentQuery = "INSERT INTO Posts (author_id, content) VALUES ('$u_id', '$content')";
 
-  if($mysqli->query($query) === TRUE){
-      echo "New record created successfully";
+    if($mysqli->query($u_idQuery) == TRUE){
 
-  }
-  else{
-        echo "Error: the username, " . $u_id . ", is already in use";
 
-  }
+          if($mysqli->query($contentQuery) == TRUE){
+
+
+            echo " Your post: " . $content . " <br> was sucessfully created";
+
+          }//end if
+          else{
+                echo "Error: Your post " . $content . ", was not uploaded";
+
+          }//end else
+
+    }//end if
+
+
+    else{
+
+          echo "Error: the username, " . $u_id . ", does not exist";
+
+    }//end else
+
+      
 $mysqli->close();
 
 ?>
